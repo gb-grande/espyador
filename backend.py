1867aca2b0cb3426c27df8a1e95cb1b73a2d6078
LelioMarcos <leliomrcunha@gmail.com>
2022-09-19 14:50:38 -0300

    Continuacao

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pegador
 app = Flask(__name__, static_folder="front")
 
 filepath="index.html"
@@ -6,3 +7,7 @@ filepath="index.html"
 @app.route("/")
 def index():
     return app.send_static_file(filepath)
+
+@app.route("/palavras/<username>", methods=["GET"])
+def palavras(username):
+    return pegar(username, 99)
